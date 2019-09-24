3a1e9aa7bb9815b61a12956aaba2e74325742e0d
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2019-09-24 09:12:57 +0300

    .env for external API url handling

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -4,6 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`
+});
+
+console.log(process.env.WAW_API_URL);
+
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
     const myData = {
         key: 123,
